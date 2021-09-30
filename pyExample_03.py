40eeb85807d6b51ba91d17bd317cf0cdb824742a
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-09-30 20:42:33 +0900

    Update pyExample_03.py

--- pyExample_03.py
+++ pyExample_03.py
@@ -1,9 +1,26 @@
 # Boolean Type Data
 
 # In the case of Boolean type data, the first letter must be capitalized.
-# 1 is returned to 'True' and 0 is returned to 'False'.
 
 print(True) # True
 print(False) # False
+
+# 1 is returned to 'True' and 0 is returned to 'False'.
+
 print(bool(0)) # False
-print(bool(123)) # True
+print(bool(1)) # True
+
+# All nonzero numbers return 'True'.
+
+print(bool(1.1)) # True
+print(bool(-2)) # True
+
+# Whether the value is 'True' or 'False' can return the bool can be returned.
+
+print(3 > 5) # False
+print(10 > 3) # True
+
+# If you put "not", the bool will be reversed.
+
+print(not False) # True
+print(not 3 < 5) # False 
