534cab84aeba9271459e888ff247ac449c9df55e
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-09-30 18:23:32 +0900

    Create pyExample_03.py

---
+++ pyExample_03.py
@@ -0,0 +1,9 @@
+# Boolean Type Data
+
+# In the case of Boolean type data, the first letter must be capitalized.
+# 1 is returned to 'True' and 0 is returned to 'False'.
+
+print(True) # True
+print(False) # False
+print(bool(0)) # False
+print(bool(123)) # True
