be8be47ebe8b3f20baf726c58ef762dd2096837b
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-09-30 16:26:14 +0900

    Create pyExample_01.py

---
+++ pyExample_01.py
@@ -0,0 +1,8 @@
+print(5) # 5 ( int )
+print(-10) # -10 ( int )
+print(3.14) # 3.14 ( float )
+print(12 + 8) # 20 ( int )
+print(3 + 4.9) # 7.9 ( float )
+print(4 * 5) # 20 ( int )
+print(5 / 2) # 2.5 ( float )
+print(5 * (7 + 6)) # 45 ( int )
