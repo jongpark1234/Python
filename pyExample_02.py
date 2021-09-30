88b1b387e815cc154066c3cf71496400c2b1a248
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-09-30 18:15:20 +0900

    Update pyExample_02.py

---
+++ pyExample_02.py
@@ -0,0 +1,6 @@
+# String Type Data
+
+print('JongPark') # JongPark
+print("ParkJong") # ParkJong
+print("!@#$%^&*()") # !@#$%^&*()
+print("JPark" * 10) # JParkJParkJParkJParkJParkJParkJParkJParkJParkJPark
