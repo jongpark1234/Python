1b78e0b3071fe3aa27bcd4f3a6d765b42f0ef6a6
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-01 08:43:08 +0900

    Update pyExmaple_04.py

--- pyExmaple_04.py
+++ pyExmaple_04.py
@@ -2,5 +2,14 @@
 
 # My name is JongPark and I am 17 years old.
 
-name = "JongPark" # ( string )
+name = "JongPark" # ( str )
 age = 17 # ( int )
+
+print(name) # JongPark
+print(age) # 17
+
+# Variables can be used together with basic characters.
+
+print("Name : " + name) # Name : JongPark
+print("Age : " + str(age)) # Age : 17
+print("Name : " + name + ", Age : " + str(age)) # Name : JongPark, Age : 17
