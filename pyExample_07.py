94738e0fc8396b4e204d6045518277e7cd547404
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-01 12:12:09 +0900

    Create pyExample_07.py

---
+++ pyExample_07.py
@@ -0,0 +1,22 @@
+# Assignment Operator
+
+# The assignment operator stores the value of the right term on the left.
+
+num = 0 # Save 0 in num ( substitution )
+
+num = num + 5 # Save 'num + 5' in num ( num = 0 + 5 = 5 )
+print(num) # 5
+num += 5 # Store the value of num + 5 in num ( It is the same as 'num = num + 5' )
+print(num) # 10
+num -= 3
+print(num) # 7
+num *= 5
+print(num) # 35
+num /= 7
+print(num) # 5.0
+num %= 3
+print(num) # 2.0
+num **= 5
+print(num) # 32.0
+num //= 4
+print(num) # 8.0
