cab2b789a3199afb498ca5afe4142e031ae29c83
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-01 10:44:17 +0900

    Update pyExample_05.py

--- pyExample_05.py
+++ pyExample_05.py
@@ -1,2 +1,7 @@
 # Comment
 
+# In this way, an explanation that does not affect the code is called a comment.
+
+# In Python, the sentence is commented by adding '#' in front of the sentence.
+
+# You can easily comment by dragging the part and pressing the 'ctrl + /'.
