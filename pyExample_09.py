6a76d1a855000f8a59308c540a341320bed868f1
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-02 02:03:35 +0900

    Create pyExample_09.py

---
+++ pyExample_09.py
@@ -0,0 +1,10 @@
+# Library
+# It allows us to take and use external functions, not Python's basic built-in functions.
+
+import math # import library
+
+print(math.ceil(300.4)) # input library name and input function.
+
+from math import *
+
+print(ceil(300.4)) # Simple.
