30aab634e4b7826a376a90ebe0144772bd0ab6d2
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-04 00:02:45 +0900

    Create pyExample_11.py

---
+++ pyExample_11.py
@@ -0,0 +1,13 @@
+# String
+
+A = "My name is JongPark."
+B = 'I am 17 years old.'
+C = """
+Python is 
+the strongest language on Earth.
+"""
+
+print(A) # My name is JongPark.
+print(B) # I am 17 years old.
+print(C) 
+
