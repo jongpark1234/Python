6194a408b8610d6369bddbc40a8001047fbb0e7e
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-03 23:57:36 +0900

    Create pyExample_10.py

---
+++ pyExample_10.py
@@ -0,0 +1,21 @@
+# Random Library
+
+import random # import random library
+
+print(random.random()) # Return an any float value 0 or higher and less than 1.
+print(random.random() * 10) # Return an any float value 0 or higher and less than 10.
+print(random.random() * 100) # Return an any float value 0 or higher and less than 100.
+
+print(random.random() * 100) # Return an any float value 0 or higher and less than 100.
+print(int(random.random() * 100)) # Return an any int value 0 or higher and less than 100.
+print(int(random.random() * 10) + 1) # Return an any int value 1 or higher and 10 or lower.
+
+print(random.randrange(100)) # Return an any int value 0 or higher and less than 100.
+print(random.randrange(1, 5)) # Return an any int value 1 or higher and less than 5.
+
+print(random.randint(0, 99)) # Return an any int value 0 or higher and 99 or lower.
+
+
+
+
+
