640fba96860dd84952f54f68dd4bf70e3b11d46a
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-03 17:54:58 +0900

    Update pyExample_09.py

--- pyExample_09.py
+++ pyExample_09.py
@@ -8,3 +8,11 @@ print(math.ceil(300.4)) # input library name and input function.
 from math import *
 
 print(ceil(300.4)) # Simple.
+
+print(ceil(25.3)) # 25
+print(floor(25.6)) # 25
+print(sqrt(625)) # 25.0
+
+from random import * # import random library
+
+print(randrange(100)) # Random numbers from 0 ~ 99 are returned.
