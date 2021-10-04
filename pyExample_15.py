bc9f6ff2e312b8ddcc174aaefd0f55d494d767de
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-04 21:56:26 +0900

    Create pyExample_15.py

---
+++ pyExample_15.py
@@ -0,0 +1,23 @@
+# Escape Character
+
+print("I am\nJongPark")
+# I am
+# JongPark
+
+print("I am \"JongPark\".") # I am "JongPark". 
+# \" -> "
+
+print('I am \'JongPark\'.') # I am 'JongPark".
+# \' -> '
+
+print("C:\\Users\\DGSW\\Documents\\Github\\Python") # C:\Users\DGSW\Documents\GitHub\Python
+# \\ -> \.
+
+print("ABCDPark\rJong") # JongPark
+# Move the corsor to start pos.
+
+print("Jongg\bPark") # JongPark
+# \b = backspace
+
+print("Jong\tPark") # Jong  Park
+# \t = tab
