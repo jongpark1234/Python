a9091467b9fd78ec32413a9a66de626591bf02f1
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-05 15:54:45 +0900

    Update pyExample_27.py

--- pyExample_27.py
+++ pyExample_27.py
@@ -13,6 +13,19 @@
 # ( Output Example )
 # The standard weight for a 175 cm tall man is 67.38 kg.
 
+# ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
 def std_weight(height, gender):
-    if (gender == "Man"):
-        return round((((height / 100) ** 2) * 22), 2)
+    if (gender == "Man" or gender == "man"):
+        return round(height * height * 22, 2)
+    elif (gender == "Woman" or gender == "woman"):
+        return round(height * height * 21, 2)
+    else:
+        return "UnKnown"
+    
+height = 175
+gender = "Woman"
+
+print("The standard weight for a {} cm tall {} is {} kg.".format(height, gender, std_weight(height / 100, gender)))
+
+    
