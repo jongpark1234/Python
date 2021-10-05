af7ffa721d54684eeb7249887adac68230bfe077
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-05 13:53:55 +0900

    edit

--- pyExample_23.py
+++ pyExample_23.py
@@ -12,3 +12,11 @@ print("Waiting number : {}".format(wn)) # Waiting number : 4
 for wn in range(4): # 4 repetitions ( variable starts at 0 when declared )
     print("Waiting number : {}".format(wn + 1))
 
+for wn in range(1, 5): # 1 ~ 4 ( When the "in" condition is satisfied, the "for" statement code is executed. )
+    print("Waiting number : {}".format(wn))
+
+
+lan = ["Python", "Java", "C"]
+
+for i in lan:
+    print("I can do {}".format(i))
---
+++ pyExample_24.py
@@ -0,0 +1,16 @@
+# While
+
+import random
+
+num = 0
+
+while (num < 5): # if num is loss than 5
+    print(num) # print num
+    num += 1 # num = num + 1
+# total 5 times repetition
+
+numlist = list(range(1, 101)) # 1 ~ 100 list.
+
+while (num != 50): # Repeat until you get 50.
+    num = int(random.sample(numlist, 1))
+    print(num)
