889263c7fa6a829921b4e1d34f677143125d8f26
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-05 10:22:53 +0900

    Update pyExample_20.py

--- pyExample_20.py
+++ pyExample_20.py
@@ -5,3 +5,23 @@ print(numset) # {1, 2, 3, 4}
 
 strset1 = {"Python", "Java", "C"}
 strset2 = set(["Python", "Node.js"])
+
+# Intersection
+
+print(strset1 & strset2) # {'Python'}
+print(strset1.intersection(strset2)) # {'Python'}
+
+# Union
+
+print(strset1 | strset2) # {'C', 'Java', 'Node.js', 'Python'} ( Random order )
+print(strset1.union(strset2)) # {'C', 'Java', 'Node.js', 'Python'} ( Random order )
+
+# difference
+
+print(strset1 - strset2) # {'C', 'Java'} ( Random order )
+print(strset1.difference(strset2)) # {'C', 'Java'} ( Random order )
+
+strset1.add("C#")
+print(strset1) # {'Python', 'C#', 'Java', 'C'}
+strset1.remove("C")
+print(strset1) # 
